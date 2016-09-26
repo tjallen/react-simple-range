61a35816012762d8786cb889659ffefa8c36148d
Thom Allen <thomwork@gmail.com>
2016-09-26 16:15:39 +0100

    Add failing tests using .find to check presence of child comps

--- test/ReactSimpleRange-test.js
+++ test/ReactSimpleRange-test.js
@@ -15,8 +15,14 @@ describe('ReactSimpleRange', () => {
     // console.log(wrapper.debug());
     expect(wrapper.length).toEqual(1);
   });
-  it('contains a SliderTrack');
-  it('contains a SliderThumb');
+  it('contains a SliderTrack', () => {
+    const wrapper = shallow(<ReactSimpleRange />);
+    expect(wrapper.find('SliderFrack').length).toEqual(1);
+  });
+  it('contains a SliderThumb', () => {
+    const wrapper = shallow(<ReactSimpleRange />);
+    expect(wrapper.find('SliderBum').length).toEqual(1);
+  });
   it('has a default value');
   it('can override the default value');
   it('passes down a specified value to SliderTrack');
