ab5d4ff823bdfb5bb95ba7325c5ec175f260491d
Thom Allen <thomwork@gmail.com>
2020-12-04 05:48:18 +0000

    More prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-var path = require('path')
+var path = require("path");
 module.exports = {
-  mode: "production",
+    mode: "production",
 
-  optimization: {
-    minimize: false
-  },
+    optimization: {
+        minimize: false,
+    },
 
-  entry: {
-    'react-simple-range': './src/components/ReactSimpleRange.js',
-  },
+    entry: {
+        "react-simple-range": "./src/components/ReactSimpleRange.js",
+    },
 
-  externals: {
-    'react': 'react',
-    'react-dom': 'react-dom',
-  },
+    externals: {
+        react: "react",
+        "react-dom": "react-dom",
+    },
 
-  output: {
-    filename: 'index.js',
-    chunkFilename: '[id].chunk.js',
-    path: path.join(__dirname, 'lib'),
-    publicPath: '/',
-    libraryTarget: 'umd',
-  },
+    output: {
+        filename: "index.js",
+        chunkFilename: "[id].chunk.js",
+        path: path.join(__dirname, "lib"),
+        publicPath: "/",
+        libraryTarget: "umd",
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.(css|scss|pcss)$/,
-        use: ['style-loader', 'css-loader?modules'],
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-    ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.(css|scss|pcss)$/,
+                use: ["style-loader", "css-loader?modules"],
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            },
+        ],
+    },
 };
