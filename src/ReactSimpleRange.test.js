350d55ddf38200b14ff895b4c67cf2279e543c5e
Thom Allen <thomwork@gmail.com>
2020-12-28 18:42:47 +0000

    Update tests

--- src/ReactSimpleRange.test.js
+++ src/ReactSimpleRange.test.js
@@ -1,28 +1,22 @@
 import { render, screen, cleanup } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { fireEvent } from "@testing-library/dom";
 import React from "react";
 import { ReactSimpleRange } from "./components/ReactSimpleRange";
 
 afterEach(cleanup);
 
-// test("testerino", () => {
-//     render(<ReactSimpleRange label />);
-//     screen.debug();
-// });
-
 test("render is gucci", () => {
     render(<ReactSimpleRange />);
     expect(screen.getByTestId("wrapper-events")).toBeTruthy();
 });
-// could test onChange as below, but currently useEffect tests checks if value has changed for basic throttling so we need to wait for keyboard event implementation
 test("fires passed onChangeComplete fn when mouse clicked", () => {
     const onChange = jest.fn();
     render(<ReactSimpleRange onChangeComplete={onChange} />);
     userEvent.dblClick(screen.getByTestId("slider-track"));
     expect(onChange).toHaveBeenCalled();
 });
-test.todo("can be focused on click");
-test.todo("reacts to keyboard events when focused");
+
 test("behaves itself when given evenly divisible step value", () => {
     render(<ReactSimpleRange max={30} step={3} />);
     expect(screen.getByTestId("slider-track")).toBeTruthy();
@@ -30,6 +24,31 @@ test("behaves itself when given evenly divisible step value", () => {
 test("renders custom thumb if provided", () => {
     render(<ReactSimpleRange customThumb={<div>test</div>} />);
     expect(screen.getByText("test")).toBeTruthy();
+});
+test("(onChange) increments slider value from arrow key events w/ default step", () => {
+    let value = 0;
+    const onChange = (s) => (value = s.value);
+    render(<ReactSimpleRange onChange={onChange} />);
+    const slider = screen.getByTestId("slider-track");
+    fireEvent.keyDown(slider, {
+        keyCode: 39,
+    });
+    expect(value).toBe(1);
+});
+test.todo(
+    "(onChangeComplete) increments slider value from arrow key events w/ default step"
+);
+test("(onChange) decrements slider value from arrow key events w/ provided step", () => {
+    let value = 50;
+    const onChange = (s) => (value = s.value);
+    render(
+        <ReactSimpleRange onChange={onChange} defaultValue={50} step={10} />
+    );
+    const slider = screen.getByTestId("slider-track");
+    fireEvent.keyDown(slider, {
+        keyCode: 37,
+    });
+    expect(value).toBe(40);
 });
 test.todo("includes a label if prop provided");
 test("does not include thumb if disableThumb prop is true", () => {
